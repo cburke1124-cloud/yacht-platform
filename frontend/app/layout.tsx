889a35efbb14874080bf11bd295ab9b13d8230f8
Christopher <cburke1124@gmail.com>
2026-04-24 23:43:20 -0500

    fix: use inline script tags for GA4 so tag appears in initial HTML

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -34,13 +34,14 @@ export default function RootLayout({
         <Script id="gtm-script" strategy="beforeInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PC4GRQR2');`}</Script>
         {/* End Google Tag Manager */}
         {/* Google tag (gtag.js) */}
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-4DLZQWFYLV" strategy="afterInteractive" />
-        <Script id="gtag-init" strategy="afterInteractive">{`
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4DLZQWFYLV" />
+        <script dangerouslySetInnerHTML={{ __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-4DLZQWFYLV');
-        `}</Script>
+        `}} />
         {/* End Google tag */}
       </head>
       <body className={inter.className} suppressHydrationWarning>
