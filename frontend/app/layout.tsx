0d94f6456ddf01ee4c65bfb201638567cb304e13
Christopher <cburke1124@gmail.com>
2026-04-24 23:35:30 -0500

    feat: add Google Analytics GA4 tag (G-4DLZQWFYLV)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -33,6 +33,15 @@ export default function RootLayout({
         {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="beforeInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PC4GRQR2');`}</Script>
         {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-4DLZQWFYLV" strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-4DLZQWFYLV');
+        `}</Script>
+        {/* End Google tag */}
       </head>
       <body className={inter.className} suppressHydrationWarning>
         {/* Google Tag Manager (noscript) */}
